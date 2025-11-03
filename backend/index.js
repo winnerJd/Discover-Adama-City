86d404369bf51f3e7e63bbbbe227a9d55855e9a2
winnerJd <jossyastu@gmail.com>
2025-11-03 13:20:58 +0300

    frontend url added

--- backend/index.js
+++ backend/index.js
@@ -31,7 +31,8 @@ app.use(cors(
             "http://localhost:8080",
             "http://localhost:8081",
             "http://localhost:5173",
-            "http://127.0.0.1:5173"
+            "http://127.0.0.1:5173",
+             "https://discover-adama-city.vercel.app" 
         ],
         credentials:true
     }
